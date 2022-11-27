b2d7877e37a7d3dd8b934323bde6be6a35230d0c
Anastasiia Sukhorukova <hrlnqnn@gmail.com>
2022-11-28 01:20:21 +0200

    added meta

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+const nav = document.querySelector(".navbar-nav");
+const navToggleBtn = document.querySelector(".nav-toggle-btn");
+const navLinks = document.querySelectorAll(".nav-link");
+
+const navToggleFunc = function () {
+    nav.classList.toggle('active');
+    navToggleBtn.classList.toggle('active');
+}
+
+navToggleBtn.addEventListener('click', navToggleFunc);
+
+for (let i = 0; i < navLinks.length; i++) {
+
+    navLinks[i].addEventListener('click', navToggleFunc);
+
+}
